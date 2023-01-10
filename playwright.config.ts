804419c91230f4249f2a92b3bc1d19a83aa7f820
erandigit <hettiarachchiec@gmail.com>
2023-01-11 00:20:27 +0530

    commit 12

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['json',{  outputFile: 'test-results.json' }]],
+  reporter: [['html',{  outputFile: 'test-results.html' }],['json',{  outputFile: 'test-results.json' }]],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
