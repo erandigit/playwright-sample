8f108c9e01ffb61ca5e9171b097f7c517906b909
Your Name <you@example.com>
2023-01-10 19:16:19 +0530

    commit 4

--- tests/jenkins-homepage.spec.ts
+++ tests/jenkins-homepage.spec.ts
@@ -4,7 +4,7 @@ test('Jenkins Homepage', async ({ page, browserName }) => {
   await page.goto('https://www.jenkins.io/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Jenkins/);
+  //await expect(page).toHaveTitle(/Jenkins/);
 
   await page.screenshot({ path: 'homepage-'+browserName+'.png', fullPage: true });
 });
