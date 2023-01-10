0fc3c0f960f1a55e1fe214f0f7c83c2b548fa3d2
Your Name <you@example.com>
2023-01-10 19:40:30 +0530

    commit 5

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await page.locator('text=Get started').click();
-  await expect(page).toHaveTitle(/Getting Started/);
+  await expect(page).toHaveTitle(/Installation | Playwright/);
 });
